a8777c7a0032d0c3063f6d648943a617e9b9734f
Ruslan <ramagenoisia@gmail.com>
2020-02-17 14:02:14 +0200

    fix components structure

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,20 +1,9 @@
-// Custom main.js file
-
-// alert('main.js');
-
 $(document).ready(function(){
     console.log('Document is ready');
 
-    svg4everybody()
-    // function calc(x, b){
-    //     return x + b;
-    // }
-
-    // var tl = new TimelineMax();
-    // console.log(tl);
+    svg4everybody();
     
 
+    // var tl = new TimelineMax();
     // tl.fromTo('h2', 10, {x : '0px'}, {x: '500px', ease: Expo.easeOut})
-
-    //  console.log(calc(5,10));
 });
